7d330b41a7f0a0f7ade6d883e7bd27e5848ff33c
Dalecrypt <34703140+Dalecrypt@users.noreply.github.com>
2017-12-20 23:06:30 -0700

    Delete Seeds.cs

--- Meincrypt1/Seeding/Seeds.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Meincrypt.Seeding
-{
-    public class Seeds
-    {
-        public static readonly string keySalt = "cbv879x6cv0876xb8769876bcvxb3cxb";
-        public static readonly string ivSalt = "0x9c8b798xcv87x6c9v8xb0987xcv0b987cxvbTiusdfoy432";
-    }
-}
